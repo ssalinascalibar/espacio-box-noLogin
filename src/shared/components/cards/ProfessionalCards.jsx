37f10cc755aab8c4c4eea2cf2e97a29fb08854a5
ssalinascalibar <ssalinascalibar@gmail.com>
2025-02-18 19:00:31 -0300

    fix professionalcards.css name for vercel

--- src/shared/components/cards/ProfessionalCards.jsx
+++ src/shared/components/cards/ProfessionalCards.jsx
@@ -3,7 +3,7 @@ import { fetchProfessionals } from "../../../services/api";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import "./ProfessionalCards.css";
+import "./professionalCards.css";
 
 const ProfessionalCards = () => {
   const [professionalCards, setProfessionalCards] = useState([]);
