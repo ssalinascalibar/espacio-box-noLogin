1c028abc1f0455c25960d3d5a079e75be3e41284
ssalinascalibar <ssalinascalibar@gmail.com>
2025-03-02 14:27:18 -0300

    add list with style to videsection

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,7 +1,7 @@
 // iconos de Material Design
-import { MdOutlineEmail, MdOutlineLocationOn, MdDateRange, MdOutlineBadge, MdVolunteerActivism } from "react-icons/md";
+import { MdOutlineEmail, MdOutlineLocationOn, MdDateRange, MdOutlineBadge, MdVolunteerActivism, MdCheckCircle } from "react-icons/md";
 // iconos Font Awesome
 import { FaInstagram } from "react-icons/fa";
 
 // Exportar solo los iconos necesarios
-export { MdOutlineEmail, MdOutlineLocationOn, MdDateRange, MdOutlineBadge, MdVolunteerActivism, FaInstagram };
+export { MdOutlineEmail, MdOutlineLocationOn, MdDateRange, MdOutlineBadge, MdVolunteerActivism, FaInstagram, MdCheckCircle };
--- src/features/home/VideoSection.jsx
+++ src/features/home/VideoSection.jsx
@@ -1,7 +1,7 @@
 import ReactPlayer from "react-player";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import { MdCheckCircle } from "../../assets/icons/icons";
 import "./videoSection.css";
 
 export default function VideoSection() {
@@ -14,7 +14,12 @@ export default function VideoSection() {
           Te invitamos a descubrir nuestros espacios, diseñados para brindar confort y funcionalidad.
           </h2>
           <p>Contamos con 3 box amoblados, equipados con todo lo necesario para que puedas atender a tus pacientes con la máxima comodidad. Cada espacio ha sido pensado para ofrecer un ambiente acogedor y profesional, permitiéndote concentrarte en lo más importante: tu trabajo y el bienestar de quienes te visitan.</p>
-
+          <ul>
+            <li><MdCheckCircle/>Luz natural</li>
+            <li><MdCheckCircle />Hermosa vista</li>
+            <li><MdCheckCircle />Ambiente Silencioso</li>
+            <li><MdCheckCircle />Baño privado</li>
+          </ul>
         </Col>
         <Col xs={12} md={6} lg={7}>
           <div className="player-wrapper">
