8fd4d1f29679064c9a1b21413875db9f29c1d6a9
ssalinascalibar <ssalinascalibar@gmail.com>
2025-03-19 17:31:06 -0300

    add background to table data

--- src/features/adminPanel/AdminPanel.jsx
+++ src/features/adminPanel/AdminPanel.jsx
@@ -52,82 +52,84 @@ export default function AdminPanel() {
 
   return (
     <Container>
-      <div id="table-title">
-        <h2>Tabla de profesionales</h2>
-        <Button
-          variant="success"
-          onClick={handleShowCreateModal}
-          className="mb-4"
-        >
-          Agregar profesional
-        </Button>
-      </div>
-      <div id="professionals-table">
-        <Table striped>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nombre</th>
-              <th>Apellido paterno</th>
-              <th>Apellido materno</th>
-              <th>Rut</th>
-              <th>Fono</th>
-              <th>Email</th>
-              <th>Título</th>
-              <th>Descripción</th>
-              <th>Valor hora</th>
-              <th>Clave acceso</th>
-              <th>Imagen</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {professionals.map((p, i) => (
-              <tr key={i}>
-                <td>{p.id}</td>
-                <td>{p.name}</td>
-                <td>{p.paternal_surname}</td>
-                <td>{p.maternal_surname}</td>
-                <td>{p.rut}</td>
-                <td>{p.phone}</td>
-                <td>{p.email}</td>
-                <td>{p.title}</td>
-                <td>
-                  <div className="custom-cell-content">{p.text}</div>
-                </td>
-                <td>{p.hourly_rate}</td>
-                <td>{p.password}</td>
-                <td><img src={p.image} alt="Imagen" className="responsive-image" /></td>
-                <td>
-                  <div id="actions">
-                    {/* <MdAddCircleOutline /> */}
-                    <FaRegEdit
-                      onClick={() => {
-                        selectProfessional(p);
-                        handleShowUpdateModal();
-                      }}
-                    />
-                    <FaRegTrashAlt onClick={() => deleteProfessional(p.id)} />
-                  </div>
-                </td>
+      <div id="background-admin">
+        <div id="table-title">
+          <h2>Tabla de profesionales</h2>
+          <Button
+            variant="success"
+            onClick={handleShowCreateModal}
+            className="mb-4"
+          >
+            Agregar profesional
+          </Button>
+        </div>
+        <div id="professionals-table">
+          <Table striped>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Apellido paterno</th>
+                <th>Apellido materno</th>
+                <th>Rut</th>
+                <th>Fono</th>
+                <th>Email</th>
+                <th>Título</th>
+                <th>Descripción</th>
+                <th>Valor hora</th>
+                <th>Clave acceso</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
               </tr>
-            ))}
-            <CreateProfessionalModal
-              show={showCreateModal}
-              handleClose={handleCloseCreateModal}
-              professionals={professionals}
-              setProfessionals={setProfessionals}
-            />
-            <UpdateProfessionalModal
-              show={showUpdateModal}
-              handleClose={handleCloseUpdateModal}
-              professionals={professionals}
-              setProfessionals={setProfessionals}
-              selectedProfessional={selectedProfessional}
-              setSelectedProfessional={setSelectedProfessional}
-            />
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {professionals.map((p, i) => (
+                <tr key={i}>
+                  <td>{p.id}</td>
+                  <td>{p.name}</td>
+                  <td>{p.paternal_surname}</td>
+                  <td>{p.maternal_surname}</td>
+                  <td>{p.rut}</td>
+                  <td>{p.phone}</td>
+                  <td>{p.email}</td>
+                  <td>{p.title}</td>
+                  <td>
+                    <div className="custom-cell-content">{p.text}</div>
+                  </td>
+                  <td>{p.hourly_rate}</td>
+                  <td>{p.password}</td>
+                  <td><img src={p.image} alt="Imagen" className="responsive-image" /></td>
+                  <td>
+                    <div id="actions">
+                      {/* <MdAddCircleOutline /> */}
+                      <FaRegEdit
+                        onClick={() => {
+                          selectProfessional(p);
+                          handleShowUpdateModal();
+                        }}
+                      />
+                      <FaRegTrashAlt onClick={() => deleteProfessional(p.id)} />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+              <CreateProfessionalModal
+                show={showCreateModal}
+                handleClose={handleCloseCreateModal}
+                professionals={professionals}
+                setProfessionals={setProfessionals}
+              />
+              <UpdateProfessionalModal
+                show={showUpdateModal}
+                handleClose={handleCloseUpdateModal}
+                professionals={professionals}
+                setProfessionals={setProfessionals}
+                selectedProfessional={selectedProfessional}
+                setSelectedProfessional={setSelectedProfessional}
+              />
+            </tbody>
+          </Table>
+        </div>
       </div>
     </Container>
   );
