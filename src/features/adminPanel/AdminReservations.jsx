d1c2f06cd6f9636ef5b5eb0608591ef130649211
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-01 15:56:07 -0300

    add payment function to adminReservations tabs

--- src/features/adminPanel/AdminReservations.jsx
+++ src/features/adminPanel/AdminReservations.jsx
@@ -1,8 +1,15 @@
 import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
 
-
-export default function AdminReservations({ professionals }) {
-
+export default function AdminReservations({ professionals, setProfessionals }) {
+    
+  const addPayment = (id) => {
+    const index = professionals.findIndex(
+      (professional) => professional.id === id
+    );
+    professionals[index].ispayed = !professionals[index].ispayed;
+    setProfessionals([...professionals]);
+  };
 
   return (
     <div id="background-admin">
@@ -27,11 +34,19 @@ export default function AdminReservations({ professionals }) {
               <tr key={i}>
                 <td>{p.id}</td>
                 <td>Sala 1</td>
-                <td>{p.name} {p.paternal_surname} {p.maternal_surname}</td>
+                <td>
+                  {p.name} {p.paternal_surname} {p.maternal_surname}
+                </td>
                 <td>Fecha</td>
                 <td>17:00hrs</td>
                 <td>{p.hourly_rate}</td>
-                <td>{p.ispayed}</td>
+                <td>
+                  <Form.Check
+                    type="checkbox"
+                    checked={p.ispayed}
+                    onChange={() => addPayment(p.id)}
+                  />
+                </td>
               </tr>
             ))}
           </tbody>
