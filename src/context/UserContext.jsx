2d0c71420af68cef8403088cb7160189bc01daac
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-05 00:24:50 -0300

    add useRef to fetch context professionals

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,19 +1,35 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useRef } from "react";
 import { fetchProfessionals } from "../services/api";
 
 const UserContext = createContext({});
 
 const UserContextProvider = ({ children }) => {
   const [professionals, setProfessionals] = useState([]);
-  console.log(professionals);
+  console.log('professionales', professionals);
+  const prevProfessionalsRef = useRef();
+  console.log('profesionales previos', prevProfessionalsRef.current);
 
   useEffect(() => {
     const getUsers = async () => {
       const data = await fetchProfessionals();
-      setProfessionals(data);
+
+      if (!prevProfessionalsRef.current || prevProfessionalsRef.current !== data) {
+        setProfessionals(data);
+        prevProfessionalsRef.current = data; 
+      }
     };
+
+    // Llamar a la función para obtener usuarios
     getUsers();
   }, []);
+  
+//   useEffect(() => {
+//     const getUsers = async () => {
+//       const data = await fetchProfessionals();
+//       setProfessionals(data);
+//     };
+//     getUsers();
+//   }, []);
   
 
   return (
