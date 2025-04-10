050e0059e8b357c32e9a97c80dd2104dab7d31f0
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-10 18:34:27 -0400

    add semantic tags to featuresection

--- src/features/home/FeatureSection.jsx
+++ src/features/home/FeatureSection.jsx
@@ -4,24 +4,26 @@ import "./featureSection.css";
 export default function FeatureSection() {
   return (
     <div className="features backgroundSection">
-      <div className="features-content">
+      <section className="features-content">
         <h2 className="section-titles">
           Ven a conocer EspacioBox Servicio de arriendo de box por horas y
           jornadas para profesionales.
         </h2>
-        <p>
-          Sabemos que el trabajo independiente puede ser solitario y contar con
-          redes de apoyo es fundamental para brindar un buen servicio centrado
-          en la ética para todos nuestros consultantes, dónde también
-          consideremos a la persona del terapeuta o especialista como parte
-          central de nuestro trabajo.
-        </p>
-        <p>
-          Invitamos a todos quienes trabajan con nosotros a formar parte de esta
-          comunidad como una red de apoyo, autocuidado y derivación.
-        </p>
-      </div>
-      <div className="fluid-image">
+        <article>
+          <p>
+            Sabemos que el trabajo independiente puede ser solitario y contar con
+            redes de apoyo es fundamental para brindar un buen servicio centrado
+            en la ética para todos nuestros consultantes, dónde también
+            consideremos a la persona del terapeuta o especialista como parte
+            central de nuestro trabajo.
+          </p>
+          <p>
+            Invitamos a todos quienes trabajan con nosotros a formar parte de esta
+            comunidad como una red de apoyo, autocuidado y derivación.
+          </p>
+        </article>
+      </section>
+      <div className="fluid-image image-features">
         <Image src="/assets/img/featuresSection.jpg" fluid />
       </div>
     </div>
