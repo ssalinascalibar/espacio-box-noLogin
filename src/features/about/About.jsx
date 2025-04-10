d0d8d52c9c5aa543a2802b2243b258a9c8db5e62
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-09 21:44:23 -0400

    add section and article tags to about.jsx

--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -6,32 +6,37 @@ export default function About() {
   return (
     <main>
       <Container>
-        <div className="backgroundSection">
-          <Row>
-            <Col lg={6}>
-              <h2>Bienvenidos a nuestra comunidad de profesionales</h2>
-            </Col>
-            <Col lg={6}>
-              <p>
-                En EspacioBox buscamos ser más que un espacio donde atender a
-                sus pacientes con comodidad sino también construir una comunidad
-                de profesionales que puedan apoyarse entre sí.
-              </p>
-              <p>
-                Sabemos que el trabajo independiente puede ser solitario y
-                contar con redes de apoyo es fundamental para brindar un buen
-                servicio centrado en la ética para todos nuestros consultantes,
-                dónde también consideremos a la persona del terapeuta o
-                especialista como parte central de nuestro trabajo.
-              </p>
-              <p>
-                Invitamos a todos quienes trabajan con nosotros a formar parte
-                de esta comunidad como una red de apoyo, autocuidado y
-                derivación.
-              </p>
-            </Col>
-          </Row>
-        </div>
+        <section>
+          <div className="backgroundSection">
+            <Row>
+              <Col lg={6}>
+                <h2>Bienvenidos a nuestra comunidad de profesionales</h2>
+              </Col>
+              <Col lg={6}>
+                <article>
+                  <p>
+                    En EspacioBox buscamos ser más que un espacio donde atender
+                    a sus pacientes con comodidad sino también construir una
+                    comunidad de profesionales que puedan apoyarse entre sí.
+                  </p>
+                  <p>
+                    Sabemos que el trabajo independiente puede ser solitario y
+                    contar con redes de apoyo es fundamental para brindar un
+                    buen servicio centrado en la ética para todos nuestros
+                    consultantes, dónde también consideremos a la persona del
+                    terapeuta o especialista como parte central de nuestro
+                    trabajo.
+                  </p>
+                  <p>
+                    Invitamos a todos quienes trabajan con nosotros a formar
+                    parte de esta comunidad como una red de apoyo, autocuidado y
+                    derivación.
+                  </p>
+                </article>
+              </Col>
+            </Row>
+          </div>
+        </section>
       </Container>
     </main>
   );
