705991bf28d8af9d5230b54143ac1cd13996e423
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-15 11:39:13 -0400

    mod user register form and mod styles forms

--- src/features/auth/UserLogin.jsx
+++ src/features/auth/UserLogin.jsx
@@ -2,22 +2,24 @@ import { useState, useEffect, useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { fetchUsers } from "../../services/api";
-import { FaUserCircle } from "../../assets/icons/icons"
+import { FaUserCircle } from "../../assets/icons/icons";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Alert from 'react-bootstrap/Alert';
+import Alert from "react-bootstrap/Alert";
+import UserRegister from "./UserRegister";
 import "./login.css";
 
 export default function UserLogin() {
-
-  const { setIsAuthUser, registeredUser, setRegisteredUser } = useContext(AuthContext);
+  const { setIsAuthUser, registeredUser, setRegisteredUser } =
+    useContext(AuthContext);
   // const [registeredUser, setRegisteredUser] = useState({});
   const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false); // Estado para alternar entre login y registro
 
   const navigate = useNavigate();
 
@@ -62,52 +64,68 @@ export default function UserLogin() {
 
   return (
     <div className="login-background">
-      <Container className="login-container">
+      <Container>
         <Row>
-        <Col md={2} lg={3}></Col>
+          <Col md={2} lg={3}></Col>
           <Col xs={12} md={8} lg={6}>
             <div className="login-form">
-            <FaUserCircle />
-              <Form onSubmit={handleSubmit}>
-                <h2>EspacioBox</h2>
-                <h4>Ingresar como Usuario</h4>
-                {successMessage && (
-                  <Alert variant="success" className="mb-4">
-                    {successMessage}
-                  </Alert>
-                )}
-                {error && (
-                  <Alert variant="danger" className="mb-4">
-                    {error}
-                  </Alert>
-                )}
-                <Form.Group controlId="email" className="mb-4">
-                  {/* <Form.Label>Correo Electrónico</Form.Label> */}
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    value={registeredUser.email || ""}
-                    onChange={handleChange}
-                    placeholder="@email"
-                    autoComplete="new-email"
-                  />
-                </Form.Group>
-                <Form.Group controlId="passsword" className="mb-4">
-                  {/* <Form.Label>Contraseña</Form.Label> */}
-                  <Form.Control
-                    type="password"
-                    name="password"
-                    value={registeredUser.password || ""}
-                    onChange={handleChange}
-                    placeholder="Contraseña"
-                    autoComplete="new-password"
-                  />
-                </Form.Group>
-                {error && <p style={{ color: "#fff" }}>{error}</p>}
-                <Button variant="primary" type="submit" size="lg">
-                  Iniciar Sesión
-                </Button>
-              </Form>
+              <FaUserCircle />
+              {isRegistering ? (
+                <UserRegister
+                  setIsRegistering={setIsRegistering}
+                  setUsers={setUsers}
+                />
+              ) : (
+                <Form onSubmit={handleSubmit}>
+                  <h2>EspacioBox</h2>
+                  <h4>Ingresar como Usuario</h4>
+                  {successMessage && (
+                    <Alert variant="success" className="mb-4">
+                      {successMessage}
+                    </Alert>
+                  )}
+                  {error && (
+                    <Alert variant="danger" className="mb-4">
+                      {error}
+                    </Alert>
+                  )}
+                  <Form.Group controlId="email" className="mb-4">
+                    {/* <Form.Label>Correo Electrónico</Form.Label> */}
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      value={registeredUser.email || ""}
+                      onChange={handleChange}
+                      placeholder="@email"
+                      autoComplete="new-email"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="passsword" className="mb-4">
+                    {/* <Form.Label>Contraseña</Form.Label> */}
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      value={registeredUser.password || ""}
+                      onChange={handleChange}
+                      placeholder="Contraseña"
+                      autoComplete="new-password"
+                    />
+                  </Form.Group>
+                  {error && <p style={{ color: "#fff" }}>{error}</p>}
+                  <Button variant="primary" type="submit" size="lg">
+                    Iniciar Sesión
+                  </Button>
+                  <p>
+                    ¿No tienes una cuenta?{" "}
+                    <Button
+                      variant="link"
+                      onClick={() => setIsRegistering(true)}
+                    >
+                      Regístrate aquí
+                    </Button>
+                  </p>
+                </Form>
+              )}
             </div>
           </Col>
           <Col md={2} lg={3}></Col>
---
+++ src/features/auth/UserRegister.jsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+
+export default function UserRegister({ setIsRegistering, setUsers }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    paternal_surname: "",
+    maternal_surname: "",
+    email: "",
+    password: "",
+    phone: "",
+    profession: "",
+    rut: "",
+  });
+
+  console.log("formData", formData);
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    // Validación simple
+    if (!formData.name || !formData.paternal_surname || !formData.maternal_surname || !formData.email || !formData.password || !formData.phone || !formData.profession || !formData.rut) {
+      setErrorMessage("Por favor, completa todos los campos.");
+      return;
+    }
+
+    // Simular agregar el usuario a la lista de usuarios
+    setUsers((prevUsers) => [...prevUsers, formData]);
+
+    setSuccessMessage("Registro exitoso. Ahora puedes iniciar sesión.");
+    setErrorMessage("");
+
+    // Limpiar el formulario
+    setFormData({
+      name: "",
+      paternal_surname: "",
+      maternal_surname: "",  
+      email: "",
+      password: "",
+      phone: "",
+      profession: "",
+      rut: "",
+    });
+
+    // Volver al formulario de inicio de sesión después de unos segundos
+    setTimeout(() => {
+      setIsRegistering(false);
+    }, 3000);
+  };
+
+  return (
+    <Form onSubmit={handleRegister}>
+        <h2>EspacioBox</h2>
+      <h4>Registro de Usuario</h4>
+      {successMessage && <Alert variant="success">{successMessage}</Alert>}
+      {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
+      <Form.Group controlId="name" className="mb-4">
+        <Form.Control
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Nombre"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Form.Group controlId="paternal_surname" className="mb-4">
+        <Form.Control
+          type="text"
+          name="paternal_surname"
+          value={formData.paternal_surname}
+          onChange={handleChange}
+          placeholder="Apellido Paterno"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Form.Group controlId="maternal_surname" className="mb-4">
+        <Form.Control
+          type="text"
+          name="maternal_surname"
+          value={formData.maternal_surname}
+          onChange={handleChange}
+          placeholder="Apellido Materno"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      
+      <Form.Group controlId="rut" className="mb-4">
+        <Form.Control
+          type="text"
+          name="rut"
+          value={formData.rut}
+          onChange={handleChange}
+          placeholder="RUT"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Form.Group controlId="email" className="mb-4">
+        <Form.Control
+          type="email"
+          name="email"
+          value={formData.email || ""}
+          onChange={handleChange}
+          placeholder="@email"
+          required
+          autoComplete="off"
+          
+        />
+      </Form.Group>
+      <Form.Group controlId="password" className="mb-4">
+        <Form.Control
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Contraseña"
+          required
+          autoComplete="new-password"
+        />
+      </Form.Group>
+      <Form.Group controlId="phone" className="mb-4">
+        <Form.Control
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          placeholder="Teléfono"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Form.Group controlId="profession" className="mb-4">
+        <Form.Control
+          type="text"
+          name="profession"
+          value={formData.profession}
+          onChange={handleChange}
+          placeholder="Profesión"
+          required
+          autoComplete="off"
+        />
+      </Form.Group>
+      <Button variant="success" type="submit" size="lg">
+        Registrarse
+      </Button>
+      <p>
+        ¿Ya tienes una cuenta?{" "}
+        <Button variant="link" onClick={() => setIsRegistering(false)}>
+          Inicia sesión aquí
+        </Button>
+      </p>
+    </Form>
+  );
+}
