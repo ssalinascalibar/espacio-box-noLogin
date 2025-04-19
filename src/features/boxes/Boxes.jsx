71c8689870bae8703e669f0f951158b871d38d72
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-19 19:51:40 -0400

    add section box features with styles

--- src/features/boxes/Boxes.jsx
+++ src/features/boxes/Boxes.jsx
@@ -3,12 +3,13 @@ import { fetchBoxes } from "../../services/api";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 // import Button from 'react-bootstrap/Button';
 import "./boxes.css";
 import ReactPlayer from "react-player";
 import ImageGallery from "react-image-gallery";
 import { GalleryShowBtn } from "./BoxesBtn";
-import { FaImages } from "../../assets/icons/icons";
+import { FaImages, FaWifi, FaCoffee, FaFan, MdOutlineLocationOn, FaKitchenSet, RiSofaFill } from "../../assets/icons/icons";
 import { ReserveLink } from "../../shared/components/links/CustomLinks";
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -23,6 +24,45 @@ export default function Boxes() {
     };
     getBoxes();
   }, []);
+
+  const iconData = [
+    {
+      id: 0,
+      icon: <RiSofaFill size={40} />,
+      title: 'Sala de espera',
+      description: 'Para que tus pacientes puedan esperar cómodamente',
+    },
+    {
+      id: 1,
+      icon: <FaWifi size={40} />,
+      title: 'WIFI',
+      description: 'Internet inalámbrico disponible en toda la oficina',
+    },
+    {
+      id: 2,
+      icon: <FaCoffee size={40} />,
+      title: 'Servicio de cafetería',
+      description: 'Café, té e infusiones a libre disposición para ti y tus pacientes',
+    },
+    {
+      id: 3,
+      icon: <FaFan size={40} />,
+      title: 'Climatización',
+      description: 'Ventiladores, aire acondicionado y calefactores para todas las temporadas',
+    },
+    {
+      id: 4,
+      icon: <FaKitchenSet size={40} />,
+      title: 'Cocina',
+      description: 'Un espacio para preparar tu comida y descansar entre tus sesiones',
+    },
+    {
+      id: 5,
+      icon: <MdOutlineLocationOn size={40} />,
+      title: 'Excelente ubicación',
+      description: 'Literalmente a pasos de Metro Los Leones',
+    },
+  ];
 
   const videoBoxes = [
     {
@@ -55,27 +95,27 @@ export default function Boxes() {
     <>
       <main>
         <Container>
-          <section>
+          <section id="box-features">
           <div id="boxes-gallery" className="backgroundSection">
             <Row>
               <Col xs={12} lg={12}>
-                <h2 style={{ textAlign: "left" }} className="section-titles">
+                <h2 className="section-titles mt-4">
                   Todo lo que necesitas para brindar una atención de calidad
                 </h2>
-                <p style={{ textAlign: "left" }}>
+                <p>
                   Espacios diseñados para que tú y tus consultantes vivan una experiencia cómoda, segura y acogedora.
                 </p>
-                <p style={{ textAlign: "left" }}>
+                <p>
                   Y sí... ¡con excelente ubicación!
                 </p>
-                <p style={{ textAlign: "left" }}>
+                <p>
                   Conoce nuestros box disponibles y encuentra tu espacio ideal para trabajar con tranquilidad. 
                 </p>
               </Col>
             </Row>
             <Row>
               <Col>
-              <div id="box-features">
+              <div id="box-features-header">
                 <h2>
                   Salas completamente equipadas con todo lo necesario para que tu sesión de terapia
                   sea una gran experiencia.
@@ -83,6 +123,23 @@ export default function Boxes() {
                 <p>Y con una excelente ubicación</p>
               </div>
               </Col>
+            </Row>
+            <Row className="g-4"> 
+            {iconData.map((card) => (
+              <Col key={card.id} xs={6} lg={4}>
+                <Card className="text-left h-100">
+                  <Card.Body className="d-flex flex-column justify-content-center">
+                  <div className="mb-3">{card.icon}</div>
+                    <Card.Title>
+                      {card.title}
+                    </Card.Title>
+                    <Card.Subtitle className="card-subtitle">
+                      {card.description}
+                    </Card.Subtitle>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
             </Row>
           </div>
           </section>
