fd5e7991d57fc893203d781a8f8c3c29c0da1291
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-20 19:07:36 -0400

    mod grid features section

--- src/features/home/FeatureSection.jsx
+++ src/features/home/FeatureSection.jsx
@@ -1,31 +1,44 @@
 import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./featureSection.css";
 
 export default function FeatureSection() {
   return (
-    <div className="features backgroundSection">
+    <div className="backgroundSection">
+      <Row>
+
+      <Col xs={12} md={6} lg={6}>
       <section className="features-content">
         <h2 className="section-titles">
-          Ven a conocer EspacioBox Servicio de arriendo de box por horas y
-          jornadas para profesionales.
+          ¿Cómo funcionamos?
         </h2>
         <article>
+          <h4>Reservar un box</h4>
           <p>
-            Sabemos que el trabajo independiente puede ser solitario y contar con
-            redes de apoyo es fundamental para brindar un buen servicio centrado
-            en la ética para todos nuestros consultantes, dónde también
-            consideremos a la persona del terapeuta o especialista como parte
-            central de nuestro trabajo.
+            En nuestro calendario online, puedes ver la disponibilidad de salas en tiempo real y reservar todas las horas que necesites.
           </p>
+          <h4>Pagos</h4>
           <p>
-            Invitamos a todos quienes trabajan con nosotros a formar parte de esta
-            comunidad como una red de apoyo, autocuidado y derivación.
+            Una vez selecionada las horas podras realizar el pago mediante transferencia y confirmar tu reserva
+          </p>
+          <h4>Cancelar horas y reembolso</h4>
+          <p>
+            Podras anular tus reservas con un minimo de 24hrs de anticipacion para poder reagendar sin volver apagar.
+          </p>
+          <h4>Acceso a la oficina </h4>
+          <p>
+          La oficina cuenta con una cerradura inteligente (con contraseña) en su puerta de acceso principal. Cuando realizas un pedido recibirás un correo de confirmación en el cual se encuentra la clave para que puedas acceder a la oficina. 
           </p>
         </article>
       </section>
-      <div className="fluid-image image-features">
+      </Col>
+      <Col xs={12} md={6} lg={6}>
+      <div className="image-features">
         <Image src="/assets/img/featuresSection.jpg" fluid />
       </div>
+      </Col>
+      </Row>
     </div>
   );
 }
