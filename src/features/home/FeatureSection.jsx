b6156adc1f08096793a7afa9659d03d76c98fb58
ssalinascalibar <ssalinascalibar@gmail.com>
2025-04-20 21:33:36 -0400

    add hr tag to como funcionamos

--- src/features/home/FeatureSection.jsx
+++ src/features/home/FeatureSection.jsx
@@ -15,18 +15,22 @@ export default function FeatureSection() {
         </h2>
         <article>
           <h4>Reservar un box</h4>
+          <hr />
           <p>
             En nuestro calendario online, puedes ver la disponibilidad de salas en tiempo real y reservar todas las horas que necesites.
           </p>
           <h4>Pagos</h4>
+          <hr />
           <p>
             Una vez selecionada las horas podras realizar el pago mediante transferencia y confirmar tu reserva
           </p>
           <h4>Cancelar horas y reembolso</h4>
+          <hr />
           <p>
             Podras anular tus reservas con un minimo de 24hrs de anticipacion para poder reagendar sin volver apagar.
           </p>
           <h4>Acceso a la oficina </h4>
+          <hr />
           <p>
           La oficina cuenta con una cerradura inteligente (con contraseña) en su puerta de acceso principal. Cuando realizas un pedido recibirás un correo de confirmación en el cual se encuentra la clave para que puedas acceder a la oficina. 
           </p>
