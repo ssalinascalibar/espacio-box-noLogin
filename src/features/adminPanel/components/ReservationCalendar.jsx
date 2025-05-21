e990cf2b1340f67dc988b9bfd8d7777ecb8d6f37
ssalinascalibar <ssalinascalibar@gmail.com>
2025-05-21 19:05:53 -0400

    fix calendar styles

--- src/features/adminPanel/components/ReservationCalendar.jsx
+++ src/features/adminPanel/components/ReservationCalendar.jsx
@@ -6,65 +6,78 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import esES from "date-fns/locale/es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./reservationCalendar.css";
 
-const locales = { es: esES };
-
-const localizer = dateFnsLocalizer({
-  format: (date, formatStr, options) =>
-    format(date, formatStr, { ...options, locale: esES }),
-  parse: (value, formatStr) =>
-    parse(value, formatStr, new Date(), { locale: esES }),
-  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
-  getDay,
-  locales,
-});
-
-const mapBookingToEvents = (bookings) => {
-  return bookings.map((booking) => {
-    const start = new Date(`${booking.date}T${booking.start_time}`);
-    const end = new Date(start.getTime() + 60 * 60 * 1000);
-
-    return {
-      id: booking.id,
-      title: `${booking.room} - ${booking.name} ${booking.paternal_surname} - ${booking.location}`,
-      start,
-      end,
-    };
-  });
-};
 
 export default function ReservationCalendar({ bookings = [] }) {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [currentView, setCurrentView] = useState("week"); //  controla la vista
+  const [currentView, setCurrentView] = useState("month"); //  controla la vista
+  
+  const locales = { es: esES };
+  
+  const localizer = dateFnsLocalizer({
+    format: (date, formatStr, options) =>
+      format(date, formatStr, { ...options, locale: esES }),
+    parse: (value, formatStr) =>
+      parse(value, formatStr, new Date(), { locale: esES }),
+    startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+    getDay,
+    locales,
+  });
+  
+  const mapBookingToEvents = (bookings) => {
+    return bookings.map((booking) => {
+      const start = new Date(`${booking.date}T${booking.start_time}`);
+      const end = new Date(start.getTime() + 60 * 60 * 1000);
+  
+      return {
+        id: booking.id,
+        title: `${booking.room}`,
+        location: `${booking.location}`,
+        name: `${booking.name} ${booking.paternal_surname} ${booking.maternal_surname}`,
+        start,
+        end,
+      };
+    });
+  };
 
-  const reservas = mapBookingToEvents(bookings);
+  const bookingEvents = mapBookingToEvents(bookings);
 
-  //   const reservas = [
-  //     {
-  //       id: 1,
-  //       title: 'Juan Pérez - Sala A',
-  //       start: new Date(2025, 4, 17, 10, 0),
-  //       end: new Date(2025, 4, 17, 11, 0),
-  //     },
-  //     {
-  //       id: 2,
-  //       title: 'Ana Gómez - Sala B',
-  //       start: new Date(2025, 4, 17, 14, 0),
-  //       end: new Date(2025, 4, 17, 15, 30),
-  //     },
-  //     {
-  //       id: 3,
-  //       title: 'Carlos Ruiz - Sala A',
-  //       start: new Date(2025, 5, 18, 9, 30),
-  //       end: new Date(2025, 5, 18, 10, 30),
-  //     },
-  //   ];
+  const components = {
+    event: (props) => {
+      console.log(props);
+      const eventType =  props?.event?.location;
+
+      const startTime = new Date(props?.event?.start).toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+      });
+  
+      const endTime = new Date(props?.event?.end).toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+      });
+      switch (eventType) {
+        case 'Providencia':
+          return <div className="custom-event custom-event-color-1">
+              {props.event.title} | {props.event.location} {startTime}-{endTime} | {props.event.name} 
+            </div>
+        case 'Las Condes':
+          return <div className="custom-event custom-event-color-2">
+              {props.event.title} | {props.event.location} {startTime}-{endTime} | {props.event.name} 
+            </div>
+          
+    }
+    }
+  }
 
   return (
-    <div style={{ height: "95vh", width: "95vw" }}>
+    <div style={{ height: "100%", width: "100%" }}>
       <Calendar
         localizer={localizer}
-        events={reservas}
+        events={bookingEvents}
         startAccessor="start"
         endAccessor="end"
         date={currentDate}
@@ -84,16 +97,17 @@ export default function ReservationCalendar({ bookings = [] }) {
           day: "Día",
           agenda: "Agenda",
           noEventsInRange: "No hay reservas",
-          showMore: (total) => `+${total} más`
+          showMore: (total) => `+${total} más`,
         }}
         formats={{
           dayFormat: (date, culture, localizer) =>
-            localizer.format(date, "eeee", culture),
+            localizer.format(date, "eee", culture),
           weekdayFormat: (date, culture, localizer) =>
             localizer.format(date, "EEEEEE", culture),
           monthHeaderFormat: (date, culture, localizer) =>
             localizer.format(date, "LLLL yyyy", culture),
         }}
+        components={components}
       />
     </div>
   );
